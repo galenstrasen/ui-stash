5fa68dd6f76cdf9e5d43c4aacf59dfc24fa86f4a
Galen Strasen <galen@wearetmbr.com>
2020-02-20 15:48:29 -0800

    Setup node modules and firebase config

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,10 @@
-
+// firebase
+const firebase = require('firebase');
+require('firebase/database');
+require('firebase/app');
 const firebaseConfig = require('./keys');
+firebase.initializeApp(firebaseConfig);
+
+// jquery
+const $ = require('jquery'); 
+
