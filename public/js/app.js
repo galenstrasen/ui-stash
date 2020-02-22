36d73847b3df835826670511ebcc6fb3f9b002bf
Galen Strasen <galen@wearetmbr.com>
2020-02-21 18:21:48 -0800

    Utilize masonry grid library

--- public/js/app.js
+++ public/js/app.js
@@ -42,3 +42,13 @@ $('#new-stash').submit((e) => {
 function createGridItem(url, tags, notes) {
     const ssGrid = document.querySelector('#ss-grid');
 }
+
+// Masonry Grid
+require('imagesloaded');
+require('isotope-layout');
+const Masonry = require('masonry-layout');
+
+var elem = document.querySelector('#ss-grid');
+var msnry = new Masonry( elem, {
+  itemSelector: '.js-grid-item'
+});
